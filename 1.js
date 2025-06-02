2bf6703d3fe84ff412dab4ff7fd26504cd1d26b2
jcz <xiaohuixu@126.com>
2025-06-03 00:16:12 +0800

    配置代码格式化工具

--- 1.js
+++ 1.js
@@ -1,5 +1,11 @@
-console.log("Hello World");
+console.log('Hello World')
 
 function add(a, b) {
-    return a + b;
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return    a * b
 }
